698917e5104def973a6c3be5f6502c53305ea590
Sangmin Kim <skim912@gmail.com>
2022-01-23 00:10:55 -0500

    test gene names

--- test/ntdaogene-test.js
+++ test/ntdaogene-test.js
@@ -19,50 +19,62 @@ describe("NTDaoGene", function () {
     await gene.deployed();
     
   });
+  describe("nft address", () => {
+    it("Should provide the correct nft address", async function () {
+      expect(await gene.getNftAddr()).to.equal(nftAddr.address);
+    });  
+  
+    it("Should match the updated nft address", async function () {
+      await gene.setNftAddr(addr1.address);
+      expect(await gene.getNftAddr()).to.equal(addr1.address);
+    });  
+  })
 
-  it("Should provide the correct nft address", async function () {
+  describe("seed", () => {
+    it("getSeed() should return seed", async function () {
+      expect((await gene.getSeed(1)).isZero()).to.equal(false);
+      expect((await gene.getSeed(1000)).isZero()).to.equal(false);
+    });  
+  
+    it("getSeed() should work only with NftAddr or Owner address", async function () {
+      await expect(gene.connect(addr1).getSeed(1)).to.eventually.be.rejectedWith(Error);
+      await expect(gene.connect(nftAddr).getSeed(1)).to.eventually.be.not.rejectedWith(Error);
+      await expect(gene.connect(owner).getSeed(1)).to.eventually.be.not.rejectedWith(Error);
+    });  
+  });
 
-    expect(await gene.getNftAddr()).to.equal(nftAddr.address);
+  describe("base genes", () => {
+    it("getBaseGenes() should work only with NftAddr or Owner address", async function () {
+      await expect(gene.connect(addr1).getBaseGenes(1)).to.eventually.be.rejectedWith(Error);
+      expect(await gene.connect(nftAddr).getBaseGenes(1)).to.be.an('array');
+      expect(await gene.connect(owner).getBaseGenes(1000)).to.be.an('array');
+    });  
+  });
 
-  });  
+  describe("gene names", () => {
+    const classes = ["Common","Rare", "Super Rare", "Treasure","National Treasure"];
+    const elements = ["Water", "Wood", "Fire", "Earth", "Metal"];
+    const branches = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat","Monkey","Rooster","Dog","Pig"];
+    const divisions = ["Geumgang", "Seorak", "Jiri", "Halla", "Baekdu"];    
+    const countries = ["Joseon","Goryeo","Balhae","Silla","Gaya","Baekje","Goguryeo","Gojoseon"];
 
-  it("Should match the updated nft address", async function () {
+    it("getBaseGeneNames() should work only with NftAddr or Owner address", async function () {
+      await expect(gene.connect(addr1).getBaseGeneNames(1)).to.eventually.be.rejectedWith(Error);
+      expect(await gene.connect(nftAddr).getBaseGeneNames(1)).to.be.an('array');
+      expect(await gene.connect(owner).getBaseGeneNames(1000)).to.be.an('array');
+  
+    });
+  
+    it("getBaseGeneNames() should return gene names", async () => {
+      const geneNames = await gene.getBaseGeneNames(1);
+  
+      expect(classes).that.includes(geneNames[0]);
+      expect(elements).that.includes(geneNames[1]);
+      expect(branches).that.includes(geneNames[2]);
+      expect(divisions).that.includes(geneNames[3]);
+      expect(countries).that.includes(geneNames[4]);
+    });
 
-    await gene.setNftAddr(addr1.address);
-    expect(await gene.getNftAddr()).to.equal(addr1.address);
-
-  });  
-
-  it("getSeed() should return seed", async function () {
-
-    expect((await gene.getSeed(1)).isZero()).to.equal(false);
-    expect((await gene.getSeed(1000)).isZero()).to.equal(false);
-
-  });  
-
-  it("getSeed() should work only with NftAddr or Owner address", async function () {
-
-    await expect(gene.connect(addr1).getSeed(1)).to.eventually.be.rejectedWith(Error);
-    await expect(gene.connect(nftAddr).getSeed(1)).to.eventually.be.not.rejectedWith(Error);
-    await expect(gene.connect(owner).getSeed(1)).to.eventually.be.not.rejectedWith(Error);
-
-  });  
-
-  it("getBaseGenes() should work only with NftAddr or Owner address", async function () {
-
-    await expect(gene.connect(addr1).getBaseGenes(1)).to.eventually.be.rejectedWith(Error);
-    expect(await gene.connect(nftAddr).getBaseGenes(1)).to.be.an('array');
-    expect(await gene.connect(owner).getBaseGenes(1000)).to.be.an('array');
-
-  });  
-
-  it("getBaseGeneNames() should work only with NftAddr or Owner address", async function () {
-
-    await expect(gene.connect(addr1).getBaseGeneNames(1)).to.eventually.be.rejectedWith(Error);
-    expect(await gene.connect(nftAddr).getBaseGeneNames(1)).to.be.an('array');
-    expect(await gene.connect(owner).getBaseGeneNames(1000)).to.be.an('array');
-
-  });  
-
+  });
 });
 
